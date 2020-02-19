57052ed5211c896e0041c9373648683f5a548a46
AVBezhenaur <bejena-ru@ya.ru>
2020-02-20 00:37:33 +0700

    step4

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ install: npm install
 start: 
 		npx node bin/brain-games.js
 
+lint:
+		npx eslint .
+
 publish:
 		npm publish --dry-run
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const main = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May i habe you name?:');
-    console.log(`Hello, ${userName}!`);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May i habe you name?:');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default main;
