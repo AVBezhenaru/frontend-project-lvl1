3bf9a1cab9b08040046c13245662713fa346fe86
AVBezhenaur <bejena-ru@ya.ru>
2020-02-23 01:09:46 +0700

    step5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 import readlineSync from 'readline-sync';
 
-const main = () => {
+const brainGames = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May i habe you name?:');
   console.log(`Hello, ${userName}!`);
 };
 
-export default main;
+const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May i habe you name?:');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const randomRange = 100;
+  let count = 0;
+
+  while (count !== 3) {
+    const randomNumber = Math.floor(Math.random() * randomRange);
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (randomNumber % 2 === 0 && answer === 'yes') {
+      count += 1;
+      console.log('Correct!');
+    } else if (randomNumber % 2 !== 0 && answer === 'no') {
+      count += 1;
+      console.log('Correct!');
+    } else if (randomNumber % 2 === 0 && answer === 'no') {
+      console.log(`"no" is wrong answer ;(. Correct answer was "yes".
+      Let's try again, ${userName}`);
+      return false;
+    } else if (randomNumber % 2 !== 0 && answer === 'yes') {
+      console.log(`"yes" is wrong answer ;(. Correct answer was "no".
+      Let's try again, ${userName}`);
+      return false;
+    } else {
+      console.log('incorrect response syntax, try again');
+      return false;
+    }
+
+    if (count === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+  return true;
+};
+
+export { brainGames, brainEven };
